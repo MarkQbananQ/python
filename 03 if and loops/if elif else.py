2d59ffde47442fcef6bcf37a839384fe4e5e99bd
MarkQbananQ <msuvorov2005@gmail.com>
2022-01-01 20:04:46 +0100

    if or for 1

--- 03 if and loops/if elif else.py
+++ 03 if and loops/if elif else.py
@@ -7,6 +7,8 @@ if num >= 3:
 if num == 5:
     print("5 równe jest 5")
 
+num = 20
+
 if num == 1:
     print("num równe jest 1")
 elif num == 2:
@@ -15,3 +17,20 @@ elif num == 5:
     print("num równe jest 5")
 elif num == 10:
     print("num równe jest 10")
+elif num >= 11:
+    print("num większe równe jest 11")
+else:
+    print("domyślny kod jeśli reszta porównań da false")
+
+
+if 5 > 1: print("5 jest wieksze od 1")
+
+if 10 > 2:
+    print("10 > 2")
+    if 4 > 1:
+        print("4 > 1")
+        if 3 > 2:
+            print("3 > 2")
+        print("4 > 1 ciąg dalszy")
+
+print("\nDalszy kod")
