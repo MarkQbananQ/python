89372089a1b5083357522a6ca5eba1fd1a5e9334
MarkQbananQ <msuvorov2005@gmail.com>
2022-01-16 15:39:18 +0100

    pycharm

--- 04 functions/arguments order.py
+++
@@ -1,6 +0,0 @@
-def printCar(brand, / , name = "concept", * , year = 1960, color = "black"):
-    print(brand, name, year, color)
-
-# printCar("Ford", "Mustang", color = "blue", year = 1973, )
-printCar( "Ford", name = "Mustang", color = "blue", year = 1973 )
-# printCar( "Ford", name = "Mustang", "blue", year = 1973 ) błąd
