d939c63944395aaf025516292a1f5184be8b448e
MarkQbananQ <msuvorov2005@gmail.com>
2022-01-16 13:33:10 +0100

    test.py

---
+++ 04 functions/test.py
@@ -0,0 +1,8 @@
+c = input("Do you agree? ")
+
+if c == 'y' or c == 'Y':
+    print("Agreed")
+elif c == 'n' or 'N':
+    print("Disagreed")
+else:
+    print("Type 'y', 'Y' to agree and 'n', 'N' to disagree.")
